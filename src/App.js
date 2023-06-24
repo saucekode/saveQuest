1cc7c46f813ef09bf3265bb646d0a0bd3f61577a
saucekode <saucekoder@gmail.com>
2023-06-24 10:11:56 +0100

    save quest init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+
+  return (
+    <div className="App">
+        hello worldd
+    </div>
+  );
+}
+
+export default App;
